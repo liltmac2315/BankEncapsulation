4bdd2ea59101df0d69699c53d2d3e3ab9ab24749
tommy santos <tommy1santos@yahoo.com>
2023-12-05 05:45:11 -0500

    BankEncapsulation/program finished.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() 
+        { 
+        
+        }
+
+
+        private double balance = 0;
+
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+        
+
+        // encapsulation
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        //Define a method named GetBalance that will return the amount stored in the balance field
+
+        public string GetBalance()
+        {
+            return  $"${balance}";
+        }
+
+
+        //public double Balance 
+        //{ 
+        //get 
+        //{ 
+        //return balance;
+        //}
+        //set 
+        //{ 
+        //balance = value;
+        //} 
+
+
+    }
+}
